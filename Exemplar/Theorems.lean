0e4b9f16a99a8928ca5f6316404fa54140500d3d
James Hetherington <j.hetherington@ucl.ac.uk>
2025-07-29 15:37:27 +0100

    Tidy

--- Exemplar/Theorems.lean
+++ Exemplar/Theorems.lean
@@ -3,30 +3,30 @@ set_option pp.parens true
 
 namespace J
 
-theorem def_add_one : ∀ (n m : Nat),  n + (m + 1) = (n + m) + 1 := by
+theorem def_add_one : ∀ (n m : ℕ),  n + (m + 1) = (n + m) + 1 := by
   intros
   rfl -- this is one of the definitions of addition
 
-theorem zero_add : ∀ n : Nat, n + 0 = n := by
+theorem zero_add : ∀ n : ℕ, n + 0 = n := by
   intros
   rfl -- this is one of the definitions of addition
 
-theorem add_zero : ∀ n : Nat, n = 0 + n := by
+theorem add_zero : ∀ n : ℕ, 0 + n = n := by
   intro n
   induction n with
     | zero => rfl
-    | succ n hyp => rw [def_add_one 0 n, <-hyp]
+    | succ n hyp => rw [def_add_one 0 n, hyp]
 
 #check ℕ
 
-theorem add_one_left : ∀ (n m : Nat),  (n + 1) + m  = (n + m) + 1 := by
+theorem add_one_left : ∀ (n m : ℕ),  (n + 1) + m  = (n + m) + 1 := by
   intro n m
   induction m with
-    | zero => rfl
+    | zero => rfl -- use both definitions of addition
     | succ m ih => rw [def_add_one n m, def_add_one (n+1) m,  ih]
 
-theorem comm : ∀ (n m : Nat), n + m = m + n := by
+theorem comm : ∀ (n m : ℕ), n + m = m + n := by
   intro n m
   induction n with
-    | zero => rw [zero_add, <- add_zero]
+    | zero => rw [zero_add, add_zero]
     | succ n ih => rw [add_one_left, ih, <- def_add_one]
